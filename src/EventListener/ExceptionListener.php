db2c8c99d5ebb023bc5e18339e39684b32b0fe90
nicola-filannino <filannino.nicola9@gmail.com>
2025-02-13 19:42:39 +0100

    refactor (ExceptionsListener) handle error 503

--- src/EventListener/ExceptionListener.php
+++ src/EventListener/ExceptionListener.php
@@ -2,10 +2,14 @@
 
 namespace App\EventListener;
 
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Doctrine\ORM\Query\QueryException;
 use Symfony\Component\HttpFoundation\Response;
+use Doctrine\DBAL\Exception\ConnectionException;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Validator\Exception\ValidationFailedException;
 
 class ExceptionListener
@@ -13,44 +17,54 @@ class ExceptionListener
     public function onKernelException(ExceptionEvent $event)
     {
         $exception = $event->getThrowable();
-        $statusCode = Response::HTTP_INTERNAL_SERVER_ERROR;
+        //dd($exception instanceof ConnectionException);
+        $statusCode = $exception instanceof \Symfony\Component\HttpKernel\Exception\HttpException
+            ? $exception->getStatusCode()
+            : Response::HTTP_INTERNAL_SERVER_ERROR;
         $message = 'An error occurred while processing your request.';
         $details = null;
 
         switch (true) {
+            // 404
             case $exception instanceof NotFoundHttpException:
                 $statusCode = Response::HTTP_NOT_FOUND;
-                $message = $exception->getMessage() ?: 'Resource not found.';
+                $message = 'Resource not found.';
                 break;
-
+            // 405
             case $exception instanceof MethodNotAllowedHttpException:
                 $statusCode = Response::HTTP_METHOD_NOT_ALLOWED;
-                $message = $exception->getMessage() ?: 'Method not allowed.';
+                $message = 'Method not allowed.';
                 break;
-
+            // 400
             case $exception instanceof BadRequestHttpException:
                 $statusCode = Response::HTTP_BAD_REQUEST;
-                $message = $exception->getMessage() ?: 'Bad request.';
+                $message = 'Bad request.';
                 break;
-
-            case $exception instanceof DBALException:
-                $statusCode = Response::HTTP_INTERNAL_SERVER_ERROR;
-                $message = $exception->getMessage() ?: 'Database error.';
-
+            // 422
             case $exception instanceof ValidationFailedException:
-                $statusCode = Response::HTTP_BAD_REQUEST;
-                $message = $exception->getMessage() ?: 'Validation failed.';
+                $statusCode = Response::HTTP_UNPROCESSABLE_ENTITY;
+                $message = 'Validation failed.';
                 $details = $this->formatValidationErrors($exception);
                 break;
-
+            // 503
+            case $exception instanceof ConnectionException:
+                $statusCode = Response::HTTP_SERVICE_UNAVAILABLE;
+                $message = 'Database connection error';
+                break;
+            // 400
             case $exception instanceof QueryException:
                 $statusCode = Response::HTTP_BAD_REQUEST;
-                $message = 'Invalid query syntax';
+                $message = 'Invalid query';
                 break;
-
+            // 400
             case $exception instanceof \JsonException:
                 $statusCode = Response::HTTP_BAD_REQUEST;
                 $message = 'Invalid JSON format';
+                break;
+            // 500
+            case $exception instanceof DBALException:
+                $statusCode = Response::HTTP_INTERNAL_SERVER_ERROR;
+                $message = 'Internal server error.';
                 break;
         }
         $data = [
